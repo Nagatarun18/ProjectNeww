aea380ec8aa09340d8672280766f291d2ee7be61
Nagatarun CS <nagatarun.c-s@siemens.com>
2023-08-09 18:54:46 +0530

    aa

---
+++ KafkaNew/src/main/java/com/example/kafkanew/KafkaNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkanew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaNewApplication.class, args);
+	}
+
+}
---
+++ KafkaNew/src/test/java/com/example/kafkanew/KafkaNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkanew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
